da2fb3e71436c905617f26ea72fc5684eb96b33f
陈奕辰 <lesliechenyichen@gmail.com>
2020-02-07 21:54:15 +0800

    首页页面轮播图功能完成
    下一步：三个tab栏之间的切换

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,35 @@
 <template>
-<div>闲云旅游网首页页面</div>
+  <!-- 轮播图区域 -->
+  <div class="banners">
+    <el-carousel height="700px" arrow="always">
+      <el-carousel-item v-for="(item,index) in banners" :key="index">
+        <div>
+          <img :src="item" alt />
+        </div>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return{
+      banners: [
+        "http://157.122.54.189:9095/assets/images/th01.jfif",
+        "http://157.122.54.189:9095/assets/images/th02.jfif",
+        "http://157.122.54.189:9095/assets/images/th03.jfif",
+        "http://157.122.54.189:9095/assets/images/th04.jfif"
+      ]
+    }
+  }
+};
 </script>
 
-<style>
-
+<style scope  lang="less">
+.banners{
+  img{
+    width: 100%;
+  }
+}
 </style>
