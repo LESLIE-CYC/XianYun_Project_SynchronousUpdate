86ae73be51edc6e1a41e7e095fef6d8cf24954cd
陈奕辰 <lesliechenyichen@gmail.com>
2020-02-07 23:12:58 +0800

    三个tab栏功能完成
    下一步：底部说明区域

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,160 @@
 <template>
   <!-- 轮播图区域 -->
-  <div class="banners">
+  <div>
     <el-carousel height="700px" arrow="always">
       <el-carousel-item v-for="(item,index) in banners" :key="index">
-        <div>
-          <img :src="item" alt />
-        </div>
+        <div
+          class="banner"
+          :style="`background:url(${item})center center no-repeat;
+         background-size:100%`"
+        ></div>
       </el-carousel-item>
     </el-carousel>
+    <!-- 搜索框 -->
+    <div class="banner-content">
+      <div class="search-bar">
+        <!-- tab栏 -->
+        <el-row type="flex" class="search-tab">
+          <span v-for="(item,index) in options" :key="index" @click="tabClick(index)" :class="{active: current ===index}">
+            <i>{{item.text}}</i>
+          </span>
+        </el-row>
+        <!-- 输入框 -->
+        <el-row type="flex" align="middle" class="search-input">
+          <input :placeholder="options[current].placeholder" />
+          <i class="el-icon-search"></i>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return{
+    return {
       banners: [
         "http://157.122.54.189:9095/assets/images/th01.jfif",
         "http://157.122.54.189:9095/assets/images/th02.jfif",
         "http://157.122.54.189:9095/assets/images/th03.jfif",
         "http://157.122.54.189:9095/assets/images/th04.jfif"
-      ]
+      ],
+      //搜索栏数据区域
+      options: [
+        {
+          text: "攻略",
+          placeholder: "搜索城市"
+        },
+        {
+          text: "酒店",
+          placeholder: "请你输入你想要搜索的酒店"
+        },
+        {
+          text: "订票",
+          placeholder: ""
+        }
+      ],
+      current: 0
+    };
+  },
+  methods: {
+    tabClick(index) {
+      this.current = index;
     }
   }
 };
 </script>
 
-<style scope  lang="less">
-.banners{
-  img{
-    width: 100%;
+<style scope lang="less">
+.banner {
+  height: 700px;
+}
+.banner-content {
+  z-index: 9;
+  width: 1000px;
+  position: absolute;
+  left: 50%;
+  top: 45%;
+  margin-left: -500px;
+  border-top: 1px transparent solid;
+
+  .search-bar {
+    width: 552px;
+    margin: 0 auto;
+  }
+
+  .search-tab {
+    .active {
+      i {
+        color: #333;
+      }
+      &::after {
+        background: #eee;
+      }
+    }
+
+    span {
+      width: 82px;
+      height: 36px;
+      display: block;
+      position: relative;
+      margin-right: 8px;
+      cursor: pointer;
+
+      i {
+        position: absolute;
+        z-index: 2;
+        display: block;
+        width: 100%;
+        height: 100%;
+        line-height: 30px;
+        text-align: center;
+        color: #fff;
+      }
+
+      &:after {
+        position: absolute;
+        left: 0;
+        top: 0;
+        display: block;
+        content: "";
+        width: 100%;
+        height: 100%;
+        border: 1px rgba(255, 255, 255, 0.2) solid;
+        border-bottom: none;
+        transform: scale(1.1, 1.3) perspective(0.7em) rotateX(2.2deg);
+        transform-origin: bottom left;
+        background: rgba(0, 0, 0, 0.5);
+        border-radius: 1px 2px 0 0;
+        box-sizing: border-box;
+      }
+    }
+  }
+
+  .search-input {
+    width: 550px;
+    height: 46px;
+    background: #fff;
+    border-radius: 0 4px 4px 4px;
+    border: 1px rgba(255, 255, 255, 0.2) solid;
+    border-top: none;
+    box-sizing: unset;
+
+    input {
+      flex: 1;
+      height: 20px;
+      padding: 13px 15px;
+      outline: none;
+      border: 0;
+      font-size: 16px;
+    }
+
+    .el-icon-search {
+      cursor: pointer;
+      font-size: 22px;
+      padding: 0 10px;
+      font-weight: bold;
+    }
   }
 }
 </style>
