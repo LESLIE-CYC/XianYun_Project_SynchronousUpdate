84a518fa5d7c3d377e3d1d39080d4a76e6f2cc99
陈奕辰 <lesliechenyichen@gmail.com>
2020-02-07 12:23:40 +0800

    闲云旅游项目初始化

---
+++ components/footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>底部公共组件</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/uset/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>用户登录与注册页面</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
