b0444083dcc3c3f4cad1fbc3f46815ec7aa40483
陈奕辰 <lesliechenyichen@gmail.com>
2020-02-20 08:10:08 +0800

    【高德地图•引入页面功能完成】：能在页面在显示完成
    【说明】：可以做一下，当用户的光标聚焦在酒店模块时•就会自动弹出了下拉城市的列表，给用户选择

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,14 +1,100 @@
 <template>
-<!-- 这里是pages:页•展示/酒店首页•页面-->
-  <div>酒店首页</div>
+<!-- 【注：这个文件是用操作高德地图•主要文件•别看错了,那些重要的属性要多看文档吧】 -->
+<!-- 注：这个style:是操作在当前中页面•高德地图•大小尺寸•的属性，相当重要，样式可以丑但功能要强大才用 -->
+  <section style="width: 400px; height: 400px;">
+    <no-ssr>
+      <el-amap vid="amap" :plugin="plugin" class="amap-demo" :center="center" :events="events">
+        <el-amap-marker v-for="(marker, index) in markers" :position="marker.position"
+                        :key="index" :vid="index" :events="marker.events"></el-amap-marker>
+        <el-amap-circle :center="circle.center" :radius="circle.radius"
+                        :fill-opacity="0.5" fill-color="#ffb5b3" stroke-color="#ffb5b3"></el-amap-circle>
+        <el-amap-polyline :path="polyline.path"></el-amap-polyline>
+      </el-amap>
+    </no-ssr>
+  </section>
 </template>
 
 <script>
+  import * as _ from 'lodash';
 export default {
-
-}
+  data() {
+    let self = this;
+    return {
+      center: [121.59996, 31.197646],
+      events: {
+        init(map) {
+          let markers = _.cloneDeep(self.markers);
+          markers.forEach((item, index) => {
+            AMapUI.loadUI(['overlay/SimpleMarker'], function (SimpleMarker) {
+              item = new SimpleMarker({
+                iconLabel: {
+                  innerHTML: index,
+                  style: {
+                    color: '#fff'
+                  }
+                },
+                iconStyle: '#1995f5',
+                map: map,
+                position: item.position
+              });
+            });
+          });
+        }
+      },
+      lng: 0,
+      lat: 0,
+      plugin: [{
+        pName: 'Geolocation',
+        events: {
+          click: (o) => {
+            o.getCurrentPosition((status, result) => {
+              if (result && result.position) {
+                self.lng = result.position.lng;
+                self.lat = result.position.lat;
+                self.center = [self.lng, self.lat];
+                self.$nextTick();
+              }
+            });
+          }
+        },
+        buttonPosition: 'LT'
+      }],
+      markers: [
+        {
+          position: [121.59996, 31.197646],
+          events: {
+            click: () => {
+              this.$router.push({path: '/single/250'});
+            }
+          },
+          visible: true,
+          clickable: true
+        },
+        {
+          position: [122.59996, 32.197646],
+          events: {
+            click: () => {
+              this.$router.push({path: '/single/250'});
+            }
+          },
+          visible: true,
+          clickable: true
+        }
+      ],
+      circle: {
+        center: [121.59996, 31.197646],
+        radius: 6000
+      },
+      polyline: {
+        path: [[121.59996, 31.1976461], [121.5389385, 31.197646]]
+      }
+    };
+  },
+  methods: {
+  },
+  mounted() {
+  },
+  beforeDestroy() {
+  }
+};
 </script>
-
-<style>
-
-</style>
